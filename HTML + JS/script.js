c2d4810c3bbf9ab348bca490eb20213f4820c203
leehzitha <leticia.burlinski@gmail.com>
2025-09-10 09:44:13 -0300

    HTML + JS

---
+++ HTML + JS/script.js
@@ -0,0 +1,20 @@
+
+const maxNumber = () => {
+
+    let num1 = parseInt(document.getElementById('number1').value);
+    let num2 = parseInt(document.getElementById('number2').value);
+    let num3 = parseInt(document.getElementById('number3').value);
+
+    if (num1 > num2 && num1 > num3){
+        document.getElementById('result').innerHTML = `Maior: ${num1}` ;
+    }
+    else if (num2 > num1 && num2 > num3){
+        document.getElementById('result').innerHTML = `Maior: ${num2}`;
+    }
+    else if (num3 > num1 && num3 > num2){
+        document.getElementById('result').innerHTML = `Maior: ${num3}`;
+    }
+    else {
+        document.getElementById('result').innerHTML = 'Os números são iguais / Não existe um único número maior!'
+    }
+}
